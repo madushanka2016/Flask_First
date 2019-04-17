359b1305a4905f4b7b53d691737f75b9705d3125
madushanka2016 <madushankakahawa@gmail.com>
2019-04-17 21:34:55 +0530

    add databases conection

--- index.py
+++ index.py
@@ -1,10 +1,23 @@
-from flask import Flask,request,render_template
+from flask import Flask, render_template,request,flash
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+
+
+# config MqSQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#inti MySQL
+mysql = MySQL(app)
 
 
 @app.route("/")
 def index():
     return render_template('home.html')
+
 
 @app.route("/about")
 def about():
@@ -21,13 +34,21 @@ def forms():
     return render_template('forms.html')
 
 
+
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method=='POST':
-       a=request.form['fullName']
-       b=request.form['age']
-       c=request.form['gender']
-       return a+" "+b+" "+c
+       name=request.form['fullName']
+       age=request.form['age']
+       gender=request.form['gender']
+       userName=request.form['userName']
+       pw=request.form['pw']
+       # return name+" "+age+" "+gender+" "+pw+" "+userName
+       cur =mysql.connection.cursor()
+
+       cur.execute("INSERT INTO details(name) VALUES(%s)",(name))
+       # cur.close()
+       return render_template("forms.html")
     else:
         return "error"
     
